de6ef564b8e502fe21b1b48759e6017e8c7d8e3b
jeffalexander <jeffrey.alan.alexander@gmail.com>
2016-02-09 20:42:28 -0500

    Refactor the validate method to smaller methods but now there's state

--- register/src/main/java/com/jaa/games/mastermind/AttemptEngine.java
+++ register/src/main/java/com/jaa/games/mastermind/AttemptEngine.java
@@ -8,49 +8,89 @@ import java.util.List;
  * @author jeff
  */
 public class AttemptEngine {
+	
+	private static final boolean NO_MATCH = false;
+	private static final boolean FOUND_MATCH = true;
+	private List<Integer> excludedSecretCodePositions = new ArrayList<Integer>();
+	private int redCount = 0;
+	private int whiteCount = 0;
+	private CodePin secretCodePin;
+	private boolean positionMatches;
+	private boolean colorMatches;
+	private boolean foundPin;
+	private List<CodePin> attempt;
 
 	public Response validate(List<CodePin> secretCode, CodePin... attemptArgs) {
-		List<CodePin> attempt = Arrays.asList(attemptArgs);
+		attempt = Arrays.asList(attemptArgs);
 		
 		if(secretCode.size() != attempt.size()) {
 			throw new IllegalArgumentException("Attempt must have same number of pins as secret code.");
 		}
 		
-		List<Integer> excludedSecretCodePositions = new ArrayList<Integer>();
-		int redCount = 0;
-		int whiteCount = 0;
-		
+		init();
+		checkEachAttemptPin(secretCode);
+		return new Response(redCount, whiteCount);
+	}
+	
+	private void init() {
+		excludedSecretCodePositions = new ArrayList<Integer>();
+		redCount = 0;
+		whiteCount = 0;
+	}
+
+	private void checkEachAttemptPin(List<CodePin> secretCode) {
 		for (int attemptPosition = 0; attemptPosition < attempt.size(); attemptPosition++) {
 			CodePin attemptPin = attempt.get(attemptPosition);
-
-			for (int secretCodePosition = 0; secretCodePosition < secretCode.size(); secretCodePosition++) {
-				if(excludedSecretCodePositions.contains(secretCodePosition)) {
-					continue;
-				}
-				
-				CodePin secretCodePin = secretCode.get(secretCodePosition);
-				
-				boolean positionMatches = attemptPosition == secretCodePosition;
-				boolean colorMatches = attemptPin.equals(secretCodePin);
-				
-				if(positionMatches && colorMatches) {
-					redCount++;
-					excludedSecretCodePositions.add(secretCodePosition);
-					break;
-				} else if(colorMatches) {
-					// FIXME this peekAhead() seems odd. Perhaps redo algorithm to find reds, then whites
-					if(attempt.get(secretCodePosition).equals(secretCodePin)) {
-						redCount++;
-						excludedSecretCodePositions.add(secretCodePosition);
-					} else {
-						whiteCount++;
-						excludedSecretCodePositions.add(secretCodePosition);
-					}
-					break;
-				}
+			checkEachSecretCodePin(secretCode, attemptPosition, attemptPin);
+		}
+	}
+	
+	private void checkEachSecretCodePin(List<CodePin> secretCode, int attemptPosition, CodePin attemptPin) {
+		for (int secretCodePosition = 0; secretCodePosition < secretCode.size(); secretCodePosition++) {
+			if(alreadyConsumedThisPosition(secretCodePosition)) {
+				continue;
+			}
+			
+			determineMatch(secretCode, attemptPosition, attemptPin, secretCodePosition);
+			recordMatch(secretCodePosition);
+			if(foundPin) {
+				break;
 			}
 		}
-		
-		return new Response(redCount, whiteCount);
+		foundPin = false;
+	}
+	
+	private boolean alreadyConsumedThisPosition(int secretCodePosition) {
+		return excludedSecretCodePositions.contains(secretCodePosition);
+	}
+	
+	private void determineMatch(List<CodePin> secretCode, int attemptPosition, CodePin attemptPin,
+			int secretCodePosition) {
+		secretCodePin = secretCode.get(secretCodePosition);
+		positionMatches = attemptPosition == secretCodePosition;
+		colorMatches = attemptPin.equals(secretCodePin);
+	}
+	
+	private void recordMatch(int secretCodePosition) {
+		if(positionMatches && colorMatches) {
+			recordRedPin(secretCodePosition);
+			foundPin = FOUND_MATCH;
+		} else if(colorMatches) {
+			// FIXME this peekAhead() seems odd. Perhaps redo algorithm to find reds, then whites
+			if(attempt.get(secretCodePosition).equals(secretCodePin)) {
+				recordRedPin(secretCodePosition);
+			} else {
+				whiteCount++;
+				excludedSecretCodePositions.add(secretCodePosition);
+			}
+			foundPin = FOUND_MATCH;
+		} else {
+			foundPin = NO_MATCH;
+		}
+	}
+
+	private void recordRedPin(int secretCodePosition) {
+		redCount++;
+		excludedSecretCodePositions.add(secretCodePosition);
 	}
 }
